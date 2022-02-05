15316bee9ecc93065edbbdab625e1c7ad24d6f15
Philip Wahlbom <philip@wahlbom.net>
2022-02-05 17:32:36 -0500

    Commit resync

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        public static decimal CalculateTax(decimal price)
+        {
+            return price * 0.08m;
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace Classes
 {
+
+    public static class Utility
+    {
+        public static void WriteColor(this string ext, ConsoleColor background, ConsoleColor foreground)
+        {
+            Console.BackgroundColor = background;
+            Console.ForegroundColor = foreground;
+            Console.WriteLine(ext);
+        }
+    }
+
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            var tax = ClassLibrary1.Class1.CalculateTax(233.440m);
+            Console.WriteLine(tax);
+            Console.ReadLine();
+
+            "Look at the pretty console colors.".WriteColor(ConsoleColor.Red,ConsoleColor.Green);
+            //var arr = new string[5];
+            //arr.Aggregate <<<<< TaskExtensions methods
+            Console.ReadLine();
+
         }
     }
 }
